24f249f5ac1ee5bb13e3746dd2ba37a5ef487ab0
Darko Sancanin <darko.sancanin@gmail.com>
2015-05-24 18:10:15 +1000

    Added DS1307 test code.

--- g_and_c_speaker.ino
+++ g_and_c_speaker.ino
@@ -35,7 +35,48 @@ void setup(){
 
 void loop(){
   WP.check_if_unused_buffer_needs_to_be_filled();
-  
+  //get_time();
+}
+
+void get_time(){
+  tmElements_t tm;
+
+  if (RTC.read(tm)) {
+    Serial.print("Ok, Time = ");
+    print2digits(tm.Hour);
+    Serial.write(':');
+    print2digits(tm.Minute);
+    Serial.write(':');
+    print2digits(tm.Second);
+    Serial.print(", Date (D/M/Y) = ");
+    Serial.print(tm.Day);
+    Serial.write('/');
+    Serial.print(tm.Month);
+    Serial.write('/');
+    Serial.print(tmYearToCalendar(tm.Year));
+    Serial.println();
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+    delay(9000);
+  }
+  delay(1000);
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+
+void get_temperature(){
   // call sensors.requestTemperatures() to issue a global temperature
   // request to all devices on the bus
   //--Serial.print(" Requesting temperatures...");
